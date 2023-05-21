264ecbca8b195ff481f8e7c579a9fe96908ab418
AJiang <AJiang0826@gmail.com>
2023-05-21 10:59:55 +0800

    update dir name test

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/qiantai_admin/book.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/qiantai_admin/book.java
@@ -3,11 +3,11 @@ package com.example.administrator.ClothingRentalSystem.admin.qiantai_admin;
 /**
  */
 
-public class book2 {
+public class book {
     private String name;
     private int imageId;
 
-    public book2(String name, int imageId) {
+    public book(String name, int imageId) {
         this.name = name;
         this.imageId = imageId;
 
