8a1ca06dd9604023411c8521adfa7ce3bda67268
AJiang <AJiang0826@gmail.com>
2023-05-21 10:51:01 +0800

    update dir name test

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/CloseCompleteReceiver.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/CloseCompleteReceiver.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class BootCompleteReceiver extends BroadcastReceiver {
+public class CloseCompleteReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
