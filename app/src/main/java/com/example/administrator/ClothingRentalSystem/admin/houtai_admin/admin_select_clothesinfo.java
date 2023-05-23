ce5b8a9b589b28ee89e9046a84df75249a08a1c9
15niki <2205470503@qq.com>
2023-05-23 23:32:54 +0800

    zby 后台修改

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_select_clothesinfo.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_select_clothesinfo.java
@@ -1,9 +1,12 @@
 package com.example.administrator.ClothingRentalSystem.admin.houtai_admin;
 
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 
 import android.os.Bundle;
@@ -11,12 +14,23 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.administrator.ClothingRentalSystem.R;
 import com.example.administrator.ClothingRentalSystem.admin.databaseHelp;
+import com.example.administrator.ClothingRentalSystem.admin.utils.DBUtils;
+import com.example.administrator.ClothingRentalSystem.admin.utils.ItemUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * 管理员查衣服信息
@@ -24,7 +38,10 @@ import com.example.administrator.ClothingRentalSystem.admin.databaseHelp;
 
 public class admin_select_clothesinfo extends AppCompatActivity {
     private ListView listView;
-
+    private CountDownLatch countDownLatch;
+    private ResultSet rs;
+    String sql;
+    List<Map<String, Object>> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,63 +53,98 @@ public class admin_select_clothesinfo extends AppCompatActivity {
     private void init() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         listView = (ListView) findViewById(R.id.Select_Clothes_List);
-        final databaseHelp help = new databaseHelp(getApplicationContext());
-        Cursor cursor = help.querybookinfo();
-        String from[] = {"name", "type", "writer","publicer","rank","img", "price"};
-        int to[] = {R.id.Clothes_Name, R.id.Clothes_Type, R.id.Clothes_Author, R.id.Clothes_Publish, R.id.Clothes_Rank, R.id.Clothes_Info_Img, R.id.Clothes_Price};
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.admin_clothes_item1, cursor, from, to);
-        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+        countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
+        sql="Select clothes_img ,name,type,price, size from clothes_information";//查询整张租借表
+        System.out.println("sql="+sql);
+        //以下开始数据库操作，使用线程，插入用户
+        new Thread(new Runnable() {
             @Override
-            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                if (view.getId() == R.id.Clothes_Info_Img) {
-                    ImageView imageView = (ImageView) view;
-                    imageView.setImageURI(Uri.parse(cursor.getString(columnIndex)));
-                    return true;
-                } else {
-                    return false;
+            public void run() {
+                try {
+                    //获得查询结果
+                    rs= DBUtils.getSelectResultSet(sql);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    //该线程执行完毕-1
+                    countDownLatch.countDown();
                 }
             }
-        });
-        listView.setAdapter(adapter);
-        //长时间停留则跳出来是否删除
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                final long temp = l;
-                builder.setMessage("确定要删除吗？").setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        help.delbookinfo((int) temp);
-                        //删除后重新显示
-                        Cursor cursor = help.querybookinfo();
-                        String from[] = {"name", "type", "writer","publicer","rank","img"};
-                        int to[] = {R.id.Clothes_Name, R.id.Clothes_Type, R.id.Clothes_Author, R.id.Clothes_Publish, R.id.Clothes_Rank, R.id.Clothes_Info_Img};
-                        SimpleCursorAdapter adapter = new SimpleCursorAdapter(admin_select_clothesinfo.this, R.layout.admin_clothes_item1, cursor, from, to);
-                        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
-                            @Override
-                            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                                if (view.getId() == R.id.Clothes_Info_Img) {
-                                    ImageView imageView = (ImageView) view;
-                                    imageView.setImageURI(Uri.parse(cursor.getString(columnIndex)));
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            }
-                        });
-                        listView.setAdapter(adapter);
-                    }
-                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+        }).start();
+        //等待线程插入完结果，把结果集转换成一定格式，并呈现在页面上
+        try {
+            countDownLatch.await();//阻塞等待线程执行完毕
+            //根据用户查询自己的租借信息
+            String[] names1={"clothes_img","name","type","price","size"};//建立字段名结果集
+            String[] names2={"Clothes_Info_Img", "Clothes_Name", "Clothes_Type", "Clothes_Price", "Clothes_Publish"};//建立字段名结果集2 这个要和SimpleAdapter中的string[]一样
+             data = ItemUtils.getList(names1,names2,rs);//调用ItemUtils获取结果集
+            System.out.println("list="+data.toString());
+            SimpleAdapter adapter = new SimpleAdapter(
+                    admin_select_clothesinfo.this, data, R.layout.admin_clothes_item,
+                    new String[]{"Clothes_Info_Img", "Clothes_Name", "Clothes_Type", "Clothes_Price", "Clothes_Publish"},
+                    new int[]{R.id.Clothes_Info_Img, R.id.Clothes_Name, R.id.Clothes_Type, R.id.Clothes_Price, R.id.Clothes_Publish});//后两个String[] int[]数组都是borrow_item中的id
+            listView.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                return true;
-            }
-        });
+//        //长时间停留则跳出来是否删除
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                final long temp = l;
+//                builder.setMessage("确定要删除吗？").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                        countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
+//                        sql="select * from clothes_lease where flage=1";//查询整张租借表
+//                        System.out.println("sql="+sql);
+//                        //以下开始数据库操作，使用线程，插入用户
+//                        new Thread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                try {
+//                                    //获得查询结果
+//                                    rs= DBUtils.getSelectResultSet(sql);
+//                                } catch (Exception e) {
+//                                    e.printStackTrace();
+//                                }finally {
+//                                    //该线程执行完毕-1
+//                                    countDownLatch.countDown();
+//                                }
+//                            }
+//                        }).start();
+//                        //等待线程插入完结果，把结果集转换成一定格式，并呈现在页面上
+//                        try {
+//                            countDownLatch.await();//阻塞等待线程执行完毕
+//                            //根据用户查询自己的租借信息
+//                            String[] names1={"id","user_name","clothes_id","clothes_size","clothes_borrow_data"};//建立字段名结果集
+//                            String[] names2={"NumberId", "UserName", "ClothesId", "ClothesSize", "BorrowDate"};//建立字段名结果集2 这个要和SimpleAdapter中的string[]一样
+//                            List<Map<String, Object>> data = ItemUtils.getList(names1,names2,rs);//调用ItemUtils获取结果集
+//                            System.out.println("list="+data.toString());
+//                            SimpleAdapter adapter = new SimpleAdapter(
+//                                    admin_select_clothesinfo.this, data, R.layout.ad_borrow_item,
+//                                    new String[]{"NumberId", "UserName", "ClothesId", "ClothesSize", "BorrowDate"},
+//                                    new int[]{R.id.NumberId, R.id.UserName, R.id.ClothesId, R.id.ClothesSize, R.id.BorrowDate});//后两个String[] int[]数组都是borrow_item中的id
+//                            listView.setAdapter(adapter);
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//
+//
+//                    }
+//                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                    }
+//                });
+//                AlertDialog dialog = builder.create();
+//                dialog.show();
+//                return true;
+//            }
+//        });
 
 
         //listview的单击事件,修改衣服信息
@@ -100,13 +152,24 @@ public class admin_select_clothesinfo extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 //传值到修改界面
-                int i = position;
+                int i = position + 1;
+                System.out.println("i="+i);
+                String str=listView.getItemAtPosition(position).toString();
+                String[] strs=str.split(", ");
+                String name=null;
+                for (int j=0;j<strs.length;j++){
+//                    System.out.println("strs[j]="+strs[j]);
+                    if (strs[j].contains("Name"))
+                    {
+                        name=strs[j].substring(14,strs[j].length());
+                    }
+                }
+                System.out.println("1111111111111111111name="+name);
                 Intent intent = new Intent(admin_select_clothesinfo.this, admin_update_clothes.class);
                 Bundle bundle = new Bundle();
-                bundle.putInt("id", i);
+                bundle.putString("name",name);
                 intent.putExtras(bundle);
                 startActivity(intent);
-                finish();
             }
         });
     }
--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_update_clothes.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_update_clothes.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,10 +22,17 @@ import androidx.annotation.Nullable;
 import com.example.administrator.ClothingRentalSystem.R;
 import com.example.administrator.ClothingRentalSystem.admin.databaseHelp;
 import com.example.administrator.ClothingRentalSystem.admin.qiantai_admin.BaseActivity;
+import com.example.administrator.ClothingRentalSystem.admin.qiantai_admin.person_borrow;
+import com.example.administrator.ClothingRentalSystem.admin.utils.DBUtils;
+import com.example.administrator.ClothingRentalSystem.admin.utils.ItemUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * 管理员修改衣服的界面
@@ -34,26 +42,32 @@ public class admin_update_clothes extends BaseActivity implements View.OnClickLi
     private ImageButton back_bt;
     private Button add_book_bt;
     private String str;
-    private int id;
+    private String name;
     private databaseHelp helper;
     Uri uri;
     private ImageView ClothesImg;
     private EditText Et_ClothesId,Et_ClothesName,Et_ClotheStype,Et_ClothesWriter,Et_ClothesPublicer,Et_ClothesPrice,Et_ClothesRank,Et_ClothesComment;
     private Button Btn_ClothesCommit,Btn_ClothesBack;
+    private CountDownLatch countDownLatch;
+    private ResultSet rs;
+    private int rows;
 
+    String sql;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_upadte_clothes);
-
+        System.out.println("3333333333333333333333333333333333333333333333");
         initdata();//界面初始化
-        helper = new databaseHelp(getApplicationContext());
+
     }
 
     private void initdata() {
         Bundle bundle = getIntent().getExtras();
-        id = bundle.getInt("id")+1 ;
-        Log.i("cursor", "initdata: " + id);
+        name = bundle.getString("name") ;
+       // Log.i("cursor", "initdata: " +name);
+        System.out.println("sname="+name);
+
 
         ClothesImg=findViewById(R.id.ClothesImg);
         Et_ClothesId=findViewById(R.id.et_ClothesId);
@@ -68,60 +82,105 @@ public class admin_update_clothes extends BaseActivity implements View.OnClickLi
 
         Btn_ClothesCommit=findViewById(R.id.Btn_ClothesCommit);
 
-        final databaseHelp help = new databaseHelp(getApplicationContext());
-        Cursor cursor = help.querybookinfoid(id);
-        if (cursor.getCount() > 0) {
-            cursor.moveToFirst();
-            ClothesImg.setImageURI(Uri.parse(cursor.getString(cursor.getColumnIndex("img"))));
-            Et_ClothesId.setText(cursor.getString(cursor.getColumnIndex("bookid")));
-            Et_ClothesName.setText(cursor.getString(cursor.getColumnIndex("name")));
-            Et_ClothesWriter.setText(cursor.getString(cursor.getColumnIndex("writer")));
-            Et_ClotheStype.setText(cursor.getString(cursor.getColumnIndex("type")));
-            Et_ClothesPrice.setText(cursor.getString(cursor.getColumnIndex("price")));
-            Et_ClothesPublicer.setText(cursor.getString(cursor.getColumnIndex("publicer")));
-            Et_ClothesComment.setText(cursor.getString(cursor.getColumnIndex("comment")));
-           Et_ClothesRank.setText(cursor.getString(cursor.getColumnIndex("rank")));
+        countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
+        sql="select * from clothes_information where name='"+name+"'";//查询整张租借表
+        System.out.println("sql="+sql);
+        //以下开始数据库操作，使用线程，插入用户
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //获得查询结果
+                    rs= DBUtils.getSelectResultSet(sql);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    //该线程执行完毕-1
+                    countDownLatch.countDown();
+                }
+            }
+        }).start();
+        //等待线程插入完结果，把结果集转换成一定格式，并呈现在页面上
+        try {
+            countDownLatch.await();//阻塞等待线程执行完毕
+            while(rs.next()) {
+                ClothesImg.setImageURI(Uri.parse(rs.getString("clothes_img")));
+                Et_ClothesId.setText(rs.getString("id"));
+                Et_ClothesName.setText(rs.getString("name"));
+                Et_ClothesWriter.setText(rs.getString("type"));
+                Et_ClotheStype.setText(rs.getString("designer"));
+                Et_ClothesPrice.setText(rs.getString("size"));
+                Et_ClothesPublicer.setText(rs.getString("price"));
+                Et_ClothesComment.setText(rs.getString("rank"));
+                Et_ClothesRank.setText(rs.getString("comment"));
+            }
+            //Add_Borrow.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
 
         Btn_ClothesCommit.setOnClickListener(this);
         ClothesImg.setOnClickListener(this);
-//        btn_clothesback.setOnClickListener(this);
 
     }
     //对管理员输入的衣服信息进行验证，全部符合要求才能通过
     boolean testid=true,testother=true;
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch(view.getId()) {
             case R.id.Btn_ClothesCommit:
                 //ISBN为十位，且不为空
-                if ( Et_ClothesId.getText().length()!=10) {
-                    Toast.makeText(admin_update_clothes.this,"请输入10位衣服编码",Toast.LENGTH_SHORT).show();
-                    testid=false;
+                if (Et_ClothesId.getText().length() != 10) {
+                    Toast.makeText(admin_update_clothes.this, "请输入10位衣服编码", Toast.LENGTH_SHORT).show();
+                    testid = false;
                     break;
                 }
 
-                if(Et_ClothesName.getText().length()==0){
-                    Toast.makeText(admin_update_clothes.this,"请输入完整衣服信息",Toast.LENGTH_SHORT).show();
-                    testother=false;
+                if (Et_ClothesName.getText().length() == 0) {
+                    Toast.makeText(admin_update_clothes.this, "请输入完整衣服信息", Toast.LENGTH_SHORT).show();
+                    testother = false;
                     break;
                 }
-                if(testid==true&&testother==true){
+                if (testid == true&&testother == true) {
+                    System.out.println(ClothesImg.toString());
+                    countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
+                    System.out.println("进入更改数据库");
+                    sql = "update clothes_information set clothes_img='"+uri+"',id='" + Et_ClothesId.getText().toString() + "',name='" +
+                            Et_ClothesName.getText().toString() + "',type='" + Et_ClothesWriter.getText().toString()
+                            + "',designer='" + Et_ClotheStype.getText().toString() + "',size='" + Et_ClothesPrice.getText().toString()
+                            + "',price='" + Et_ClothesPublicer.getText().toString() + "',rank='" + Et_ClothesComment.getText().toString()
+                            + "',comment='" + Et_ClothesComment.getText().toString() + "'where name='" + name + "'";//查询整张租借表
+                    System.out.println("sql=" + sql);
 
-                    SQLiteDatabase db = helper.getWritableDatabase();
-                    ContentValues values = new ContentValues();
-                    values.put("name", Et_ClothesName.getText().toString());
-                    values.put("writer", Et_ClothesWriter.getText().toString());
-                    values.put("bookid", Et_ClothesId.getText().toString());
-                    values.put("price", Et_ClothesPrice.getText().toString());
-                    values.put("publicer", Et_ClothesPublicer.getText().toString());
-                    values.put("comment", Et_ClothesComment.getText().toString());
-                    values.put("rank", Et_ClothesRank.getText().toString());
-                    values.put("type",Et_ClotheStype.getText().toString());
-                    db.update("bookinfo", values, "_id=?", new String[]{String.valueOf(id)});
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //获得查询结果
+                                rows = DBUtils.getUpdateRows(sql);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                //该线程执行完毕-1
+                                countDownLatch.countDown();
+                            }
+                        }
+                    }).start();
+                    try {
+                        countDownLatch.await();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("----------------------row="+rows);
+                    if (rows > 0)
+                        Toast.makeText(admin_update_clothes.this, "修改衣服信息成功！", Toast.LENGTH_SHORT).show();
+                    else
+                        System.out.println("修改衣服失败！请重新尝试！");
+                    Intent intent = new Intent(admin_update_clothes.this, admin_select_clothesinfo.class);
+                    startActivity(intent);
+                    finish();
 
-
-                    Toast.makeText(admin_update_clothes.this,"修改衣服信息成功！",Toast.LENGTH_SHORT).show();
                     break;
                 }
 
@@ -136,6 +195,7 @@ public class admin_update_clothes extends BaseActivity implements View.OnClickLi
     }
 
     @Override
+    //用于显示图片
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
