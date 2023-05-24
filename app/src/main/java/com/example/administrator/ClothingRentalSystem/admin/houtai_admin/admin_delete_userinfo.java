d2bd396c100f1f9374cf16d41600f90659e8047f
88whui <1748609542@qq.com>
2023-05-25 01:20:21 +0800

    wxh commit 测试

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_delete_userinfo.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_delete_userinfo.java
@@ -1,7 +1,6 @@
 package com.example.administrator.ClothingRentalSystem.admin.houtai_admin;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
