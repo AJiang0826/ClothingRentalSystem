a408b3161f55d2a600770880234dcae01c20e366
15niki <2205470503@qq.com>
2023-05-24 14:19:36 +0800

    zby 后台修改

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_select_clothesinfo.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_select_clothesinfo.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -42,6 +43,8 @@ public class admin_select_clothesinfo extends AppCompatActivity {
     private ResultSet rs;
     String sql;
     List<Map<String, Object>> data;
+    private int row;
+    String clothesname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,63 +91,73 @@ public class admin_select_clothesinfo extends AppCompatActivity {
             e.printStackTrace();
         }
 
-//        //长时间停留则跳出来是否删除
-//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                final long temp = l;
-//                builder.setMessage("确定要删除吗？").setPositiveButton("确定", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//
-//                        countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
-//                        sql="select * from clothes_lease where flage=1";//查询整张租借表
-//                        System.out.println("sql="+sql);
-//                        //以下开始数据库操作，使用线程，插入用户
-//                        new Thread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                try {
-//                                    //获得查询结果
-//                                    rs= DBUtils.getSelectResultSet(sql);
-//                                } catch (Exception e) {
-//                                    e.printStackTrace();
-//                                }finally {
-//                                    //该线程执行完毕-1
-//                                    countDownLatch.countDown();
-//                                }
-//                            }
-//                        }).start();
-//                        //等待线程插入完结果，把结果集转换成一定格式，并呈现在页面上
-//                        try {
-//                            countDownLatch.await();//阻塞等待线程执行完毕
-//                            //根据用户查询自己的租借信息
-//                            String[] names1={"id","user_name","clothes_id","clothes_size","clothes_borrow_data"};//建立字段名结果集
-//                            String[] names2={"NumberId", "UserName", "ClothesId", "ClothesSize", "BorrowDate"};//建立字段名结果集2 这个要和SimpleAdapter中的string[]一样
-//                            List<Map<String, Object>> data = ItemUtils.getList(names1,names2,rs);//调用ItemUtils获取结果集
-//                            System.out.println("list="+data.toString());
-//                            SimpleAdapter adapter = new SimpleAdapter(
-//                                    admin_select_clothesinfo.this, data, R.layout.ad_borrow_item,
-//                                    new String[]{"NumberId", "UserName", "ClothesId", "ClothesSize", "BorrowDate"},
-//                                    new int[]{R.id.NumberId, R.id.UserName, R.id.ClothesId, R.id.ClothesSize, R.id.BorrowDate});//后两个String[] int[]数组都是borrow_item中的id
-//                            listView.setAdapter(adapter);
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//
-//
-//                    }
-//                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//
-//                    }
-//                });
-//                AlertDialog dialog = builder.create();
-//                dialog.show();
-//                return true;
-//            }
-//        });
+        //长时间停留则跳出来是否删除
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final long temp = l;
+                int a = i+ 1;
+                System.out.println("i="+a);
+                String str=listView.getItemAtPosition(i).toString();
+                String[] strs=str.split(", ");
+                  clothesname=null;
+                for (int j=0;j<strs.length;j++){
+//                    System.out.println("strs[j]="+strs[j]);
+                    if (strs[j].contains("Name"))
+                    {
+                        clothesname=strs[j].substring(14,strs[j].length());
+                    }
+                }
+                builder.setMessage("确定要删除吗？").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
+                        sql="delete from clothes_information where name='"+clothesname+"'";//查询整张租借表
+                        System.out.println("sql="+sql);
+                        //以下开始数据库操作，使用线程，插入用户
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    //获得查询结果
+                                    row=DBUtils.getUpdateRows(sql);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }finally {
+                                    //该线程执行完毕-1
+                                    countDownLatch.countDown();
+                                }
+                            }
+                        }).start();
+                        //等待线程插入完结果，把结果集转换成一定格式，并呈现在页面上
+                        try {
+                            countDownLatch.await();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println("----------------------row="+row);
+                        if (row < 0)
+                            Toast.makeText( admin_select_clothesinfo.this, "删除衣服信息成功！", Toast.LENGTH_SHORT).show();
+                        else
+                            System.out.println("删除衣服失败！请重新尝试！");
+                        Intent intent = new Intent( admin_select_clothesinfo.this, admin_select_clothesinfo.class);
+                        startActivity(intent);
+                        finish();
+
+
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+            }
+        });
 
 
         //listview的单击事件,修改衣服信息
