13d34d4333186bda2f8daed88edd2bf40966ca2f
15niki <2205470503@qq.com>
2023-05-24 14:53:40 +0800

    zby 后台修改

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_pay_info.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_pay_info.java
@@ -30,7 +30,7 @@ private ListView ad_pay;
         setContentView(R.layout.activity_admin_pay_info);
         ad_pay=(ListView)findViewById(R.id.Add_Show_Pay);
         countDownLatch = new CountDownLatch(1);//创建线程计时器个数是1
-        sql="select * from clothes_lease where flage=0";//查询整张租借表
+        sql="select * from clothes_lease where flage=1";//查询整张租借表，flage=1，表示衣服已经归还
         System.out.println("sql="+sql);
         //以下开始数据库操作，使用线程，插入用户
         new Thread(new Runnable() {
