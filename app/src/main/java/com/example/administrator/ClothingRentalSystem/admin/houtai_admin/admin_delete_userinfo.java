9899869947c6dda2e83ab463efb33bc93eb6e396
15niki <2205470503@qq.com>
2023-05-25 13:46:57 +0800

    zby 修改界面,进行小的调整

--- app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_delete_userinfo.java
+++ app/src/main/java/com/example/administrator/ClothingRentalSystem/admin/houtai_admin/admin_delete_userinfo.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.administrator.ClothingRentalSystem.R;
-import com.example.administrator.ClothingRentalSystem.admin.ActivityCollector;
 import com.example.administrator.ClothingRentalSystem.admin.utils.DBUtils;
 import java.sql.ResultSet;
 import java.util.concurrent.CountDownLatch;
